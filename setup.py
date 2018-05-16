935c85db79548707ada2fdb7d69ab48ad1874888
Ville M. Vainio <ville.vainio@basware.com>
2018-05-16 21:04:49 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='Scaffer',
+      version='0.0.0',
+      description='Scaffold stuff',
+      author='Ville M. Vainio',
+      author_email='vivainio@gmail.com',
+      url='https://github.com/vivainio/scaffer',
+      packages=['scaffer'],
+      install_requires=[],
+      entry_points = {
+        'console_scripts': [
+            'scaffer = scaffer.scaffer:main'
+        ]
+      }
+     )
