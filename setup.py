827b657d2e8190502bb7db100bae1db7f69b50fd
Ville M. Vainio <ville.vainio@basware.com>
2018-05-26 19:06:48 +0300

    bump version, manifest.in

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Scaffer',
-      version='0.1.0',
+      version='0.2.0',
       description='Scaffold stuff',
       author='Ville M. Vainio',
       author_email='vivainio@gmail.com',
