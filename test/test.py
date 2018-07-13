3330861698d55189980e4cfadc554205578fdfcf
Ville M. Vainio <ville.vainio@basware.com>
2018-07-13 11:44:05 +0300

    test dry runs

--- test/test.py
+++ test/test.py
@@ -6,11 +6,13 @@ def run(c):
     print(">",cmd)
     subprocess.check_call(cmd)
 
+run(["g", "s1", "-v", "bar=aa-bb", "prj=cc-dd", "-f", "--dry"])
 run(["g", "s1", "-v", "bar=aa-bb", "prj=cc-dd", "-f"])
 cont=open("cc-dd/aa-bb.txt").read()
 assert cont == 'CcDdHello vs AaBbWorld in pascalcase.\nLower kebab cc-dd-hello aa-bb-world\nLower snake cc_dd_hello aa_bb_world\nLower dotted cc.dd.hello aa.bb.world\nLower flat ccdd aabb\nUpper dotted SCF.PRJ.HELLO SCF.BAR.world\nUpper kebab SCF-PRJ-HELLO SCF-BAR-world\nUpper snake SCF_PRJ_HELLO SCF_BAR_world\nUpper flat CCDD AABB\n'
-run(["g",  "sinit", "-f"])
 
+run(["g",  "sinit", "-f", "--dry"])
+run(["g",  "sinit", "-f"])
 assert "@" in open("projectdetails.txt").read()
 
 # these should not ask anything since all scf instances are invalid
