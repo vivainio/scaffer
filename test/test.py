a5ddc429c356f7d124cc24c80403455d78eb596e
Ville M. Vainio <ville.vainio@basware.com>
2021-02-05 19:02:34 +0200

    style fixes

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ import subprocess
 def run(c):
     cmd = ["python", "-m", "scaffer"] + c
     print(">", cmd)
-    subprocess.check_call(cmd, cwd="..", )
+    subprocess.check_call(cmd)
 
 
 run(["g", "s1", "-v", "bar=aa-bb", "prj=cc-dd",
@@ -12,10 +12,7 @@ run(["g", "s1", "-v", "bar=aa-bb", "prj=cc-dd",
 run(["g", "s1", "-v", "bar=aa-bb", "prj=cc-dd",
      "spaced=Hello World", "-f"])
 cont = open("cc-dd/aa-bb.txt").read()
-assert cont == "CcDdHello vs AaBbWorld in pascalcase.\nLower kebab cc-dd-hello aa-bb-world\nLower snake cc_dd_hello " \
-               "aa_bb_world\nLower dotted cc.dd.hello aa.bb.world\nLower flat ccdd aabb\nUpper dotted SCF.PRJ.HELLO " \
-               "SCF.BAR.world\nUpper kebab SCF-PRJ-HELLO SCF-BAR-world\nUpper snake SCF_PRJ_HELLO " \
-               "SCF_BAR_world\nUpper flat CCDD AABB\nVerbatim (w/o compound handling) cc-dd aa-bb 'Hello World' "
+assert cont ==  "CcDdHello vs AaBbWorld in pascalcase.\nLower kebab cc-dd-hello aa-bb-world\nLower snake cc_dd_hello aa_bb_world\nLower dotted cc.dd.hello aa.bb.world\nLower flat ccdd aabb\nUpper dotted SCF.PRJ.HELLO SCF.BAR.world\nUpper kebab SCF-PRJ-HELLO SCF-BAR-world\nUpper snake SCF_PRJ_HELLO SCF_BAR_world\nUpper flat CCDD AABB\nVerbatim (w/o compound handling) cc-dd aa-bb 'Hello World'"
 
 run(["g", "sinit", "-f", "--dry"])
 run(["g", "sinit", "-f"])
